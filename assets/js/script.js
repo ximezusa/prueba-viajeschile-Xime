d1b3d35bcf58d2dd639e0d1231264a5516de675c
Ximena <ojedamat@gmail.com>
2024-06-29 21:37:08 -0400

    subiendo archivo index.html

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let Contacto = prompt("Contacto:");
+var elementoContacto = document.getElementById("idContacto");
+elementoContacto.innerHTML = Contacto;
+
+let Nombre = prompt("Ingrese Nombre");
+var elementoHtmlNota1 = document.getElementById("idNombre");
+elementoNombre.innerHTML = Nombre;
+
+let Nombr = prompt("ingrese su nombre")
+alert("Suscribase" + Nombre)
+
